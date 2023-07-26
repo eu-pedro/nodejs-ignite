03dd555580af07b5a34ff642e88fe17563cd05b8
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-07-26 20:03:36 -0300

    feat: separando rotas da aplicação

---
+++ src/route.js
@@ -0,0 +1,32 @@
+import { Database } from "./database.js"
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: 'GET',
+    path: '/tasks',
+    handler: (req, res) => {
+      const tasks = database.select('tasks')
+      return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'POST',
+    path: '/tasks',
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      database.insert('tasks', {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      })
+
+      return res.writeHead(201).end('criação de task')
+    }
+  }
+]
