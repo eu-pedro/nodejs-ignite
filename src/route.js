8f2123397673968970c95b8702d3bd8648798c7c
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-07-28 18:05:24 -0300

    feat: validação de title ou description vazios

--- src/route.js
+++ src/route.js
@@ -23,6 +23,12 @@ export const routes = [
     handler: (req, res) => {
       const { title, description } = req.body
 
+      if(!title || !description) {
+        return res.writeHead(404).end(JSON.stringify({
+          message: 'title and description are required'
+        }))
+      }
+
       database.insert('tasks', {
         id: randomUUID(),
         title,
